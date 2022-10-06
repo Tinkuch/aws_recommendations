0cac8132e33b7e8d4892876e1f226f216ea471a0
Tinkuch <tinku.ccc@gmail.com>
2022-10-06 15:24:53 +0530

    aws_recommendations

--- aws_recommendations.jenkins.groovy
+++ aws_recommendations.jenkins.groovy
@@ -1,7 +1,7 @@
 node('worker02'){
         stage('Git Clone'){
             git branch: 'main', url: 'https://github.com/Tinkuch/aws_recommendations'
-            sh 'chmod 777 /home/ashokdas_test1/jenkins/workspace/aws_recommendations'
+            sh 'chmod 777 /home/ashokdas_test1/jenkins/workspace/AWS_Recommendations'
         }
         stage("AWS"){
             dir ("${env.WORKSPACE}"){
