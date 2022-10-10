0cfec09566b91b4f7b774f546d051c49c5f4b488
Tinkuch <tinku.ccc@gmail.com>
2022-10-10 15:03:10 +0530

    aws_recommendations

--- aws_recommender.sh
+++ aws_recommender.sh
@@ -67,7 +67,8 @@ git push -u origin "${BRANCH}":"${BRANCH}"
 
 #git push -o merge_request.create
 
-hub pull-request -m "merge "${BRANCH}"approve merge to apply recommendation"
+hub pull-request 
+#-m "merge "${BRANCH}"approve merge to apply recommendation"
 
 #git push \
 # -o merge_request.create \
